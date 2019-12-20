57c6f446eb1fb766ed816968c1b4446fe2406f8f
lethanhtuyen1008 <53989518+lethanhtuyen1008@users.noreply.github.com>
2019-12-20 18:46:35 +0700

    Add files via upload

---
+++ Travel/envAPI.js
@@ -0,0 +1,3 @@
+export const API = "https://evening-refuge-27360.herokuapp.com/api/";
+export const APILC = "https://evening-refuge-27360.herokuapp.com/api/";
+export const APIIMG = "https://evening-refuge-27360.herokuapp.com/api/images/";
